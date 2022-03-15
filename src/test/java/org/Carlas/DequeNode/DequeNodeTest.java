336852c284378ff5c7e51eb5694d842a4e86a642
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2022-03-15 12:15:16 +0100

    creacionPrimeraPrueba

--- src/test/java/org/Carlas/DequeNode/DequeNodeTest.java
+++ src/test/java/org/Carlas/DequeNode/DequeNodeTest.java
@@ -2,12 +2,15 @@ package org.Carlas.DequeNode;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Deque;
 
 class DequeNodeTest<T> {
     private DequeNode deque;
     private T item;
-    private T next;
-    private T previous;
+    private DequeNode<T> next;
+    private DequeNode<T> previous;
     @BeforeEach
     private void setup() {
         deque = new DequeNode(item, next, previous);
@@ -16,7 +19,12 @@ class DequeNodeTest<T> {
     @AfterEach
     private void finish() {
         deque = null;
-    }  
+    }
+
+    @Test
+    public void testComputeIfNodeIsTheFirstNode() {
+
+    }
 
 
 }
