4b46b99c37372313628cd2dec6acbc17ef433e39
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2022-03-15 11:43:28 +0100

    codigoDequeNodeTest

--- src/test/java/org/Carlas/DequeNode/DequeNodeTest.java
+++ src/test/java/org/Carlas/DequeNode/DequeNodeTest.java
@@ -1,5 +1,22 @@
 package org.Carlas.DequeNode;
 
-class DequeNodeTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class DequeNodeTest<T> {
+    private DequeNode deque;
+    private T item;
+    private T next;
+    private T previous;
+    @BeforeEach
+    private void setup() {
+        deque = new DequeNode(item, next, previous);
+    }
+
+    @AfterEach
+    private void finish() {
+        deque = null;
+    }
+
 
 }
